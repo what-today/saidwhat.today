187bf977eb16072b4310476c4f9ccb75b6d5ac66
Isaac Greenspan <isaac.greenspan@vokal.io>
2016-07-27 19:21:56 -0500

    Fully working.

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+#!/usr/bin/env python
 
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+import os
 import webapp2
+import jinja2
+import yaml
+from random import choice
 
+DOMAIN = 'saidwhat.today'
+
+os.environ['TZ'] = 'America/Chicago'
+
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
+    )
+
+people = {}
 
 class MainPage(webapp2.RequestHandler):
+    def bail(self):
+        return self.redirect('http://{}/'.format(DOMAIN))
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        if self.request.path != '/':
+            return self.redirect('/')
+        if not self.request.host.endswith(DOMAIN):
+            return self.bail()
+
+        name = self.request.host[:-len(DOMAIN)-1]
+        if not name:
+            return self.response.out.write(
+                    jinja_environment.get_template('index.html').render(
+                        {
+                            }
+                        )
+                    )
+        if '/' in name or '..' in name:
+            self.bail()
+        lines = people.get(name)
+        if not lines:
+            try:
+                with open('lines/{}.yaml'.format(name)) as f:
+                    people[name] = yaml.safe_load(f)
+                lines = people[name]
+            except (IOError, KeyError):
+                return self.bail()
+        self.response.out.write(
+                jinja_environment.get_template('saidwhat.html').render(dict(
+                    name=name.capitalize(),
+                    line=choice(lines),
+                    ))
+                )
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/.*', MainPage),
 ], debug=True)
